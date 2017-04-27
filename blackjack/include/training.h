ab9074182cdc16c89f51d50f71daa1df6a8c4ca1
gbekosa <aisosaefemwonkieke@gmail.com>
2017-04-26 23:51:00 -0700

    Cleaned up

--- blackjack/include/training.h
+++
@@ -1,9 +0,0 @@
-#ifndef TRAINING_H
-#define TRAINING_H
-
-#include <iostream>
-std::string TSTAY_FILE = "STAY.DND";
-std::string THIT_FILE = "HIT.DND";
-int run_game(long long int count, bool new_data = false); // returns number of succesful games played
-#endif
-
